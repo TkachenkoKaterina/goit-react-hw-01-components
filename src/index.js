9d80ede17308392a3cb3cd4f270e0442fec4e7d9
Katerina <tkachenko.city@gmail.com>
2023-01-31 20:47:12 +0200

    hw1 in process

--- src/index.js
+++ src/index.js
@@ -3,11 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root'))
-  .render
-  // < React.StrictMode>
-  //   <App />
-  // </>
-  ();
-
-console.log(7);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
