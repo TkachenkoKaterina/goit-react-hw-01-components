85c118ad06f44b6c566557acf94a178738714ae0
Kateryna Tkachenko <Tkachenko.city@gmail.com>
2024-02-03 17:15:53 +0200

    2/4 task without css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+import Profile from './Profile/Profile';
+import Statistics from './Statistics/Statistics';
+import user from './Profile/user.json';
+import data from './Statistics/data.json';
+
+const App = () => (
+  <div>
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
+    <Statistics title="Upload stats" stats={data} />
+  </div>
+);
+
+export default App;
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,20 @@
+const Statistics = ({ title, stats }) => {
+  return (
+    <section className="statistics">
+      {title && <h2 className="title">{title}</h2>}
+
+      <ul className="stat-list">
+        {stats.map(({ label, percentage, id }) => {
+          return (
+            <li key={id} className="item">
+              <span className="label">{label}</span>
+              <span className="percentage">{percentage}</span>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+};
+
+export default Statistics;
